84054986d792c06cadfab9102a858d2acf0b199f
Geethanjali <geeth@192.168.0.213>
2023-06-13 19:37:08 +0100

    palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,6 +7,16 @@
 */
 
 function isPalindrome(str) {
+ 
+  var re=/[^A-Za-z0-9]/g;
+  str=str.toUpperCase().replace(re, '');
+  var length=str.length;
+  for(var i=0; i<length/2;i++){
+    if(str[i]!== str[length-1-i]){
+      return false;
+    }
+  }
+  
   return true;
 }
 
